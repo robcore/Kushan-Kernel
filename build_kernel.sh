f2399a9053aeefe793de710c27df72427be7c0b1
kushan02 <kushan.mehta@yahoo.com>
2015-07-02 22:18:20 +0530

    Added my own build file

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,22 @@
-mkdir $(pwd)/out
-make -C $(pwd) O=$(pwd)/out VARIANT_DEFCONFIG=jf_eur_defconfig jf_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=$(pwd)/out
-cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+#!/bin/bash
+# Kushan-Kernel Build Script
+# Copyright (C) Kushan
+
+#vars
+KERNEL_DIR=$(pwd)
+DATE="`date +"%d-%m-%Y"`"
+BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+OUTPUT=BUILD_$DATE
+
+make clean
+mkdir $KERNEL_DIR/BUILD_$DATE
+mkdir $KERNEL_DIR/BUILD_$DATE/modules
+export ARCH=arm
+export USE_SEC_FIPS_MODE=true
+export CROSS_COMPILE=/home/kushan/toolchain_linaro/bin/arm-eabi-
+export ccache=ccache
+# export ENABLE_GRAPHITE=true
+make VARIANT_DEFCONFIG=jf_eur_defconfig kushan_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j$BUILD_JOB_NUMBER
+cp -r $KERNEL_DIR/arch/arm/boot/zImage $OUTPUT/zImage
+find . -name "*.ko" -exec cp {} $OUTPUT/modules/ \;
